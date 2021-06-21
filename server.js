3a0df9e0ce68a4b227aa3af8f94687158355ee31
ishanmazhar <ishanmazhar2@gmail.com>
2021-06-21 11:29:56 +0600

    Initial Setup

---
+++ server.js
@@ -0,0 +1,21 @@
+// Connect Mongodb
+// Run Server
+
+const dotenv = require('dotenv');
+dotenv.config();
+const app = require('./app');
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGODB_SERVER, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+})
+    .then(() => console.log('Connected to MongoDB!'))
+    .catch(err => console.log("MongoDB Connection Failed!"));
+
+const port = process.env.PORT || 3000
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`); 
+})
